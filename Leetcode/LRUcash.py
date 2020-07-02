8da2c76de332d29f37731c06b5d297621d8413fd
zqiaohe <zanaevab@gmail.com>
2020-07-02 17:27:37 +0300

    Добавляю нормальную очередь

--- Leetcode/LRUcash.py
+++ Leetcode/LRUcash.py
@@ -1,75 +1,58 @@
-# структура, которая хранит значение, и ссылку на следующее значение и предыдущее
-class element:
-    Last = None
-    Next = None
-    Value = None
+# class queue():
+#     class element:
+#         last = None
+#         next = None
+#         value = None
+#         __init__(self, Value: int):
+#             self.value = Value
+#     Queue = []
+#     head = None
+#     tail = None
+#     Capacity = 0
+#     def __init__(self, capacity: int):
+#         self.Capacity = capacity
 
-    def __init__(self, value):
-        self.Value = value
-
-
-class queue():
-    head = element(-1)
-    Queue = []
-    headindex = 0
+class LRUCache:
+    Dict = {}
+    Queue = queue()
     Capacity = 0
-
     def __init__(self, capacity: int):
         self.Capacity = capacity
 
-    def Headindex(self):
-        return self.headindex
-
-    def put_element(self, element):
-        if len(self.Queue) == 0:
-            element.Last = self.head
-            self.head.Next = element
-            self.head = element
-            self.Queue.append(element)
-        elif len(self.Queue) < self.Capacity:
-            element.Last = self.head
-            self.head.Next = element
-            self.head = element
-            self.Queue.append(element)
-            self.headindex += 1
-        else:
-            element.Last = self.head
-            self.head.Next = element
-            self.head = element
-            self.Queue.append(element)
-            self.headindex += 1
-            self.Queue[self.headindex - self.Capacity].Value = -1
-
-
-class LRUCache:
-    MyDict = {}
-    MyQueue = queue(0)
-
-    def __init__(self, capacity: int):
-        self.MyQueue = queue(capacity)
-
     def get(self, key: int) -> int:
-        if self.MyDict.get(key) is None:
-            return -1
+        self.Headindex = len(self.Queue)
+        print(type(self.Dict.get(key)))
+        if self.Dict.get(key) is not None and self.Dict.get(key) > (len(self.Queue) - self.Capacity - 1) :
+            Value = self.Queue[self.Dict.get(key)]
+            self.put(key, Value)
+            return Value
         else:
-            if self.MyDict.get(key).Value == -1:
-                return -1
-            else:
-                self.put(key, self.MyDict.get(key).Value)
-                return self.MyDict.get(key).Value
+            return -1
 
     def put(self, key: int, value: int):
-        self.MyQueue.put_element(element(value))
-        self.MyDict[key] = self.MyQueue.Queue[self.MyQueue.Headindex()]
+        self.Queue.append(value)
+        self.Dict[key] = len(self.Queue)-1
 
+# cache = LRUCache(2)
+# cache.put(1, 1)
+# cache.put(2, 2)
+# print(cache.get(1))       #// returns 1
+# cache.put(3, 3)    #// evicts key 2
+# print(cache.get(2))      #// returns -1 (not found)
+# cache.put(4, 4)    #// evicts key 1
+# print(cache.get(1))       #// returns -1 (not found)
+# print(cache.get(3) )      #// returns 3
+# print(cache.get(4))       #// returns 4
 
-cache = LRUCache(2)
-cache.put(1, 1)
-cache.put(2, 2)
-print(cache.get(1))  # // returns 1
-cache.put(3, 3)  # // evicts key 2
-print(cache.get(2))  # // returns -1 (not found)
-cache.put(4, 4)  # // evicts key 1
-print(cache.get(1))  # // returns -1 (not found)
-print(cache.get(3))  # // returns 3
-print(cache.get(4))
+# cache2 = LRUCache(1)
+# cache2.put(2, 1)
+# print(cache2.get(2))
+
+# cache3 = LRUCache(2)
+# print(cache.get(2))
+# cache.put(2, 6)
+# print(cache.get(1))
+# cache.put(1, 5)
+# cache.put(1, 2)
+# print(cache.get(1))
+# print(cache.get(2))
